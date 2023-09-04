a934b6147c5a479b9f2b04dc3b15f2f98414384b
Sania Javed <sania.javed.789@gmail.com>
2023-09-04 22:33:30 +0530

    Fixed the bug related to home route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Home', url: '/', icon: 'home' },
+    { title: 'Home', url: '/home', icon: 'home' },
     { title: 'About Us', url: '/about-us', icon: 'information-circle' },
     { title: 'Contact Us', url: '/contact-us', icon: 'call' },
   ];
