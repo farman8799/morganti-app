f7863d4ca5cd61ccd00292661c381d180a8a32b8
Irfan Ali <ia.ali053263@gmail.com>
2023-09-15 15:37:40 +0530

    add interceptor

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,13 +1,11 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.scss'],
 })
 export class ContactUsComponent implements OnInit {
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   ngOnInit() {}
 }
--- src/app/interceptor/auth.interceptor.ts
+++ src/app/interceptor/auth.interceptor.ts
@@ -6,22 +6,25 @@ import {
   HttpInterceptor,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  private consumerKey = 'ck_ec268086198dd321148000cc8c28a35f85525c00';
-  private consumerSecret = 'cs_fcd73d02e6d0d53249ba5017b8d51c4e95dac8b6';
-
+  private API_URL = environment.WooCommerce.API_URL;
+  private consumer_key = environment.WooCommerce.CONSUMER_KEY;
+  private consumer_secret = environment.WooCommerce.CONSUMER_SECRET;
   constructor() {}
 
-  intercept(request: HttpRequest<any>, next: HttpHandler) {
-    const authReq = request.clone({
-      setHeaders: {
-        Authorization: `Basic ${btoa(
-          `${this.consumerKey}:${this.consumerSecret}`
-        )}`,
-      },
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    const req = request.clone({
+      url: `${this.API_URL}?consumer_key=${this.consumer_key}
+      &consumer_secret=${this.consumer_secret}`,
     });
-    return next.handle(authReq);
+    console.log(req);
+
+    return next.handle(req);
   }
 }
