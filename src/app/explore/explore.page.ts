2414c30beaa3812404debae4db497c36869913b7
MubMaj <mub.maj@gmail.com>
2023-09-26 16:08:25 +0530

    Merge branch 'dev' of https://github.com/farman8799/morganti-app into bug/create-swiper

---
+++ src/app/explore/explore.page.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-explore',
+  templateUrl: './explore.page.html',
+  styleUrls: ['./explore.page.scss'],
+})
+export class ExplorePage implements OnInit {
+  videoProperty= [
+   {
+    videoUrl: 'https://morganti.letshavewebsite.com/wp-content/uploads/2023/09/men-perfume.mp4'
+   },
+   {
+    videoUrl: 'https://morganti.letshavewebsite.com/wp-content/uploads/2023/09/Women-Perfume.mp4'
+   },
+   {
+    videoUrl: 'https://morganti.letshavewebsite.com/wp-content/uploads/2023/09/WhatsApp-Video-2023-09-11-at-7.15.20-PM.mp4'
+   }
+  ]
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  ionViewDidEnter() {
+  this.setupVideos(true);
+  }
+
+  ionViewWillEnter(){
+    this.playVideos();
+        
+  }
+
+  ionViewWillLeave() {
+    this.pauseVideos();
+  }
+
+  private setupVideos(muted: boolean) {
+    const videos = document.querySelectorAll('video');
+    videos.forEach((video) => {
+      video.muted = muted;
+      video.currentTime = 0;
+    });
+  }
+
+  private pauseVideos() {
+    const videos = document.querySelectorAll('video');
+    videos.forEach((video) => {
+      video.pause();
+    });
+  }
+
+  private playVideos() {
+    const videos = document.querySelectorAll('video');
+    videos.forEach((video) => {
+      video.play();
+    });
+  }
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,124 +8,17 @@ import { ProductService } from '../product.service';
 })
 export class HomePage implements OnInit {
   products: any[] = [];
-  // products = [
-  //   {
-  //     cardProducts: [
-  //       {
-  //         name: 'HYPNOTIC',
-  //         description: `Introducing Hypnotic by Morganti Luxury – an extraordinary
-  //         concentrated fragrance that combines the timeless elegance of Iris
-  //         and Cedarwood with the fresh and citrusy notes of Neroli and the
-  //         deep and mysterious aroma of Somalia Frankincense.`,
-  //         price: 950,
-  //         currency: 'AED',
-  //         image: './../../../../assets/image-1.svg',
-  //         logo: './../../../../assets/logo.svg'
-  //       }
-  //     ],
-  //     sliderProducts: {
-  //       title: 'EXPLORE THE FRAGRANCES AND CANDLES',
-  //       products: [
-  //         {
-  //           name: 'Salman by Morganti',
-  //           description: ``,
-  //           price: 1250,
-  //           currency: 'AED',
-  //           image: './../../../../assets/product-img.svg',
-  //           logo: ''
-  //         }
-  //       ]
-  //     }
-  //   },
-  //   {
-  //     cardProducts: [
-  //       {
-  //         name: 'HYPNOTIC',
-  //         description: `Introducing Hypnotic by Morganti Luxury – an extraordinary
-  //         concentrated fragrance that combines the timeless elegance of Iris
-  //         and Cedarwood with the fresh and citrusy notes of Neroli and the
-  //         deep and mysterious aroma of Somalia Frankincense.`,
-  //         price: 850,
-  //         currency: 'AED',
-  //         image: './../../../../assets/image-2.svg',
-  //         logo: './../../../../assets/logo.svg'
-  //       },
-  //       {
-  //         name: 'HYPNOTIC',
-  //         description: `Introducing Hypnotic by Morganti Luxury – an extraordinary
-  //         concentrated fragrance that combines the timeless elegance of Iris
-  //         and Cedarwood with the fresh and citrusy notes of Neroli and the
-  //         deep and mysterious aroma of Somalia Frankincense.`,
-  //         price: 1050,
-  //         currency: 'AED',
-  //         image: './../../../../assets/image-3.svg',
-  //         logo: './../../../../assets/logo.svg'
-  //       }
-  //     ],
-  //     sliderProducts: {
-  //       title: 'WOMENS FRAGRANCES',
-  //       products: [
-  //         {
-  //           name: 'Salman by Morganti',
-  //           description: ``,
-  //           price: 1250,
-  //           currency: 'AED',
-  //           image: './../../../../assets/product-img.svg',
-  //           logo: ''
-  //         }
-  //       ]
-  //     }
-  //   },
-  //   {
-  //     cardProducts: [
-  //       {
-  //         name: 'HYPNOTIC',
-  //         description: `Introducing Hypnotic by Morganti Luxury – an extraordinary
-  //         concentrated fragrance that combines the timeless elegance of Iris
-  //         and Cedarwood with the fresh and citrusy notes of Neroli and the
-  //         deep and mysterious aroma of Somalia Frankincense.`,
-  //         price: 850,
-  //         currency: 'AED',
-  //         image: './../../../../assets/image-4.svg',
-  //         logo: './../../../../assets/logo.svg'
-  //       },
-  //       {
-  //         name: 'HYPNOTIC',
-  //         description: `Introducing Hypnotic by Morganti Luxury – an extraordinary
-  //         concentrated fragrance that combines the timeless elegance of Iris
-  //         and Cedarwood with the fresh and citrusy notes of Neroli and the
-  //         deep and mysterious aroma of Somalia Frankincense.`,
-  //         price: 1050,
-  //         currency: 'AED',
-  //         image: './../../../../assets/image-5.svg',
-  //         logo: './../../../../assets/logo.svg'
-  //       }
-  //     ],
-  //     sliderProducts: {
-  //       title: 'MENS FRAGRANCES',
-  //       products: [
-  //         {
-  //           name: 'Salman by Morganti',
-  //           description: ``,
-  //           price: 1250,
-  //           currency: 'AED',
-  //           image: './../../../../assets/product-img.svg',
-  //           logo: ''
-  //         }
-  //       ]
-  //     }
-  //   }
-    
-  // ]
+
   constructor(private productService:ProductService) { }
 
   ngOnInit() {
-    this.productService.getCategories().subscribe(
-      res => {
-        console.log(res);
-      }
-    );
+    const products = localStorage.getItem('products');
 
+    if (products === null) this.getProducts();
+    else this.products = JSON.parse(products);
+  }
+
+  getProducts() {
     this.productService.getProducts().subscribe({
       next: (data: any) => {
         this.products = data;
@@ -140,12 +33,10 @@ export class HomePage implements OnInit {
           obj.slug = product.slug;
           obj.stock_status = product.stock_status;
           return obj;
-        })
-        console.log('Products:', this.products);
-      },
-      error: (error) => {
-        console.error('Error fetching products:', error);
-      },
+        });
+
+        localStorage.setItem('products', JSON.stringify(this.products))
+      }
     });
   }
 }
