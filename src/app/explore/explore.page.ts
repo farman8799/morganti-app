b793e1fe6d41278b028c5b95677565123c4b0b3e
firoj8630 <pro1.firoj@gmail.com>
2023-09-26 17:35:37 +0530

    icon animate and add tabs category and add content upppwe video

--- src/app/explore/explore.page.ts
+++ src/app/explore/explore.page.ts
@@ -8,13 +8,16 @@ import { Component, OnInit} from '@angular/core';
 export class ExplorePage implements OnInit {
   videoProperty= [
    {
-    videoUrl: 'https://morganti.letshavewebsite.com/wp-content/uploads/2023/09/men-perfume.mp4'
+    videoUrl: 'https://morganti.letshavewebsite.com/wp-content/uploads/2023/09/men-perfume.mp4',
+    title: 'Men Fragrance'
    },
    {
-    videoUrl: 'https://morganti.letshavewebsite.com/wp-content/uploads/2023/09/Women-Perfume.mp4'
+    videoUrl: 'https://morganti.letshavewebsite.com/wp-content/uploads/2023/09/Women-Perfume.mp4',
+    title: 'Women Fragrance'
    },
    {
-    videoUrl: 'https://morganti.letshavewebsite.com/wp-content/uploads/2023/09/WhatsApp-Video-2023-09-11-at-7.15.20-PM.mp4'
+    videoUrl: 'https://morganti.letshavewebsite.com/wp-content/uploads/2023/09/WhatsApp-Video-2023-09-11-at-7.15.20-PM.mp4',
+    title: 'Candles'
    }
   ]
 
